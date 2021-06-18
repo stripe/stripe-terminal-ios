1b9f425a342cb99ccd83a95fd6290504c53d23dc
James Little <39812724+jil-stripe@users.noreply.github.com>
2021-06-18 11:35:01 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "StripeTerminal",
-      url: "https://github.com/stripe/stripe-terminal-ios/releases/download/v2.0.0/StripeTerminal.xcframework.zip",
+      url: "https://github.com/jil-stripe/terminal-ios-public-repo-test/releases/download/v2.0.0/StripeTerminal.xcframework.zip",
       checksum: "f78e3652f5306b8882dbf67535a0c042ee2d0f03cf532d0f19ae130bebef5c3d"
     )
   ]
