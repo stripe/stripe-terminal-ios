bc6344494a880e10913eac51273e2129fb4367c4
Brian Cooke <bric@stripe.com>
2021-08-30 11:43:14 -0700

    2.1.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "StripeTerminal",
-      url: "https://github.com/stripe/stripe-terminal-ios/releases/download/v2.1.0/StripeTerminal.xcframework.zip",
-      checksum: "8fa229a487979c113e669899a59fb2f0a1592da28e20957131ffbf1f31c4678d"
+      url: "https://github.com/stripe/stripe-terminal-ios/releases/download/v2.1.1/StripeTerminal.xcframework.zip",
+      checksum: "60253d8509fd36aeb9d30f9855da80256a1f56844eabcd530a9c9d97f3c6db16"
     )
   ]
 )
