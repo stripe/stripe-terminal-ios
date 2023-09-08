3372c726abbe3e6ce1b687fdf2ecf56effe9e99f
Brian Cooke <bric@stripe.com>
2023-09-08 09:43:36 -0700

    Fix Package.swift URL (no longer includes the v prefix)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "StripeTerminal",
-      url: "https://github.com/stripe/stripe-terminal-ios/releases/download/v3.0.0/StripeTerminal.xcframework.zip",
+      url: "https://github.com/stripe/stripe-terminal-ios/releases/download/3.0.0/StripeTerminal.xcframework.zip",
       checksum: "4cf81e6795559d5663a5d903abe4b3cacd6ac4a756a8d118513a2c28b8829dd0"
     )
   ]
